92376acd4c8de9675a0db291645601d8461a49df
Tariq-act <khantarikact@gmail.com>
2023-07-21 00:33:18 +0530

    Implement Clerk platform for authencation and create sign-in/sign-up page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
     <div>
-      Dashboard Page
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
-}
+};
 
 export default DashboardPage;
